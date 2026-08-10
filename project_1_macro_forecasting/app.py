0a4582e2a4fdb6440ec797bf0c13da9777951cdb
BerlinNapoleon <berlinapoleon@gmail.com>
2026-08-11 01:41:42 +0700

    Update dummy data USD/IDR to more realistic value

--- project_1_macro_forecasting/app.py
+++ project_1_macro_forecasting/app.py
@@ -13,7 +13,7 @@ st.caption('Data Pipeline Otomatis | Proyeksi Time-Series untuk Analisis Kebijak
 # Filter & Metric Cards
 col1, col2, col3, col4 = st.columns(4)
 col1.metric('Inflasi Terakhir', '2.84%', '-0.12%')
-col2.metric('USD / IDR', 'Rp 15.650', '+Rp 45')
+col2.metric('USD / IDR', 'Rp 16.350', '+Rp 45')
 col3.metric('BI Rate', '6.25%', '0.00%')
 col4.metric('Harga Minyak (WTI)', '$74.50', '-$1.20')
 
